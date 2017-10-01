f39339ec2aac7ac5bfe010c5c5b50d7fed6c6ecf
arabian9ts <nok14.tm59@gmail.com>
2017-10-02 01:41:06 +0900

    enhance: default box generator

--- model/default_box.py
+++ model/default_box.py
@@ -5,16 +5,70 @@ date: 9/30
 author: arabian9ts
 """
 
-def generate_boxes():
+import numpy as np
+from policy import *
+
+def scale(k):
+    """
+    compute default box scale.
+    In SSD-Thesis, s_k is defined as
+        s_k = s_min + (s_max - s_min) * (k - 1.0) / (m - 1.0), m = 6
+    I corresponded with index from zero, so replace (k - 1.0) -> k.
+
+    Args: feature map number
+    Returns: scale
+    """
+    s_min = 0.1
+    s_max = 0.95
+    m = 6.0
+    s_k = s_min + (s_max - s_min) * k / (m - 1.0)
+    return s_k
+
+
+def generate_boxes(fmap_shapes):
     """
     generate default boxes based on defined number
-    the shape is [ [ first-feature-map-boxes ],
-                   [ second-feature-map-boxes ],
+    the shape is [  first-feature-map-boxes ,
+                    second-feature-map-boxes ,
                                 ...
-                   [ sixth-feature-map-boxes ], ]
-        ==> ( 6 x defined_size )
+                    sixth-feature-map-boxes , ]
+        ==> ( total_boxes_number x defined_size )
 
-    Returns: generated default boxes list
+    Args:
+        feature map sizes per output such as...
+        [ [ 19, 19, ],      # feature-map-shape 1
+          [ 19, 19, ],      # feature-map-shape 2
+          [ 10, 10 ],       # feature-map-shape 3
+          [ 5, 5, ],        # feature-map-shape 4
+          [ 3, 3, ],        # feature-map-shape 5
+          [ 1, 1, ],        # feature-map-shape 6
+        ]
+    Returns:
+        generated default boxes list
     """
-    
-    return
+
+    default_boxes = []
+
+    # this loop should be already 6 loops
+    for index, map_shape in zip(range(len(fmap_shapes)), fmap_shapes):
+        height = map_shape[1]
+        width = map_shape[2]
+        ratios = box_ratios[index]
+
+        for y in range(height):
+            for x in range(width):
+                for ratio in ratios:
+                    s_k = scale(index)
+                    s_k1 = scale(index+1)
+
+                    if 1.0 == ratio:
+                        s_k = np.sqrt(s_k*s_k1)
+
+                    center_x = (x + 0.5) / float(width)
+                    center_y = (y + 0.5) / float(height)
+                    box_width = s_k * np.sqrt(ratio)
+                    box_height = s_k / np.sqrt(ratio)
+
+                    default_boxes.append([center_x, center_y, box_height, box_width])
+                    
+    return default_boxes
