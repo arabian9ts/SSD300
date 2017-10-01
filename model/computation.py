f251d69ca645d3b094bcf051f0759ca73d10a707
arabian9ts <nok14.tm59@gmail.com>
2017-10-01 22:41:07 +0900

    enhance: intersection

--- model/computation.py
+++ model/computation.py
@@ -6,6 +6,9 @@ author: arabian9ts
 """
 
 def smooth_L1(x):
+    """
+    smooth L1
+    """
     y = 0
     if abs(x) < 1:
         y = 0.5 * x**2
@@ -14,5 +17,25 @@ def smooth_L1(x):
 
     return y
 
+def intersection(rect1, rect2):
+    """
+    intersecton of units
+    rect is defined as [ top_left_x, top_left_y, width, height ]
+    """
+    top = max(rect1[1], rect2[1])
+    left = max(rect1[0], rect2[0])
+    right = min(rect1[0] + rect1[2], rect2[0] + rect2[2])
+    bottom = min(rect1[1] + rect1[3], rect2[1] + rect2[3])
+
+    if bottom > top and right > left:
+        return (bottom-top)*(right-left)
+
+    return 0
+
+
 print(smooth_L1(1))
 print(smooth_L1(0.1))
+
+rect1 = [0, 0, 10, 10]
+rect2 = [5, 5, 10, 10]
+print(intersection(rect1, rect2))
