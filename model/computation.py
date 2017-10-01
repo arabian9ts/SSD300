6b4974dda922f2d99710c8b0a4fb5230b609d29b
arabian9ts <nok14.tm59@gmail.com>
2017-10-01 22:31:03 +0900

    enhance: smooth L1

--- model/computation.py
+++ model/computation.py
@@ -4,3 +4,15 @@ compute required indicies.
 date: 10/1
 author: arabian9ts
 """
+
+def smooth_L1(x):
+    y = 0
+    if abs(x) < 1:
+        y = 0.5 * x**2
+    else:
+        y = abs(x) - 0.5
+
+    return y
+
+print(smooth_L1(1))
+print(smooth_L1(0.1))
