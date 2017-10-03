ee2b37ab3404a972def3016815af8552fc54b5bd
arabian9ts <nok14.tm59@gmail.com>
2017-10-03 11:38:30 +0900

    enhance: NotSpecifiedException implemented

--- matcher.py
+++ matcher.py
@@ -4,3 +4,18 @@ matching function is defined here.
 date: 10/2
 author: arabian9ts
 """
+
+class Matcher:
+    def __init__(self, fmap_shapes, default_boxes):
+        if not fmap_shapes:
+            pass
+        
+    def apply_prediction(pred_confs, pred_locs, fmap_shapes):
+        return 
+
+class NotSpecifiedException(Exception):
+    def __init__(self, name, epicenter):
+        self.name = name
+        self.epicenter = epicenter
+    def __str__(self):
+        return ('Parameter "{0.name}" is not specified in "{0.epicenter}"'.format(self))
