81566baf4f61ffc675ab4fae2cba6eba05a43c5b
arabian9ts <nok14.tm59@gmail.com>
2017-10-03 17:11:19 +0900

    enhance: apply prediction

--- matcher.py
+++ matcher.py
@@ -5,15 +5,51 @@ date: 10/2
 author: arabian9ts
 """
 
+import numpy as np
+
+from model.policy import *
+
 class Matcher:
     def __init__(self, fmap_shapes, default_boxes):
         if not fmap_shapes:
             raise NotSpecifiedException('fmap_shapes', 'Matcher __init__')
         if not default_boxes:
             raise NotSpecifiedException('default_boxes', 'Matcher __init__')
+
+        self.fmap_shapes = fmap_shapes
+        self.default_boxes = default_boxes
         
-    def apply_prediction(pred_confs, pred_locs, fmap_shapes):
-        return 
+    def apply_prediction(self, pred_confs, pred_locs):
+        index = 0
+        confs = []
+        anchors = [
+            [[[None for _ in range(boxes[i])]
+            for _ in range(self.fmap_shapes[1])]
+            for _ in range(self.fmap_shapes[2])]
+            for i in range(len(boxes))
+        ]
+
+        for i in range(len(boxes)):
+            for y in range(self.fmap_shapes[1]):
+                for x in range(self.fmap_shapes[2]):
+                    for j in range(boxes[i]):
+                        offset = pred_locs[index]
+                        cx = self.default_boxes[i][y][x][j][0] + offset[0]
+                        cy = self.default_boxes[i][y][x][j][1] + offset[1]
+                        height = self.default_boxes[i][y][x][j][2] + offset[2]
+                        width = self.default_boxes[i][y][x][j][3] + offset[3]
+                        box = [cy, cy, height, width]
+                        anchors[i][y][x][j] = box
+                        pred_conf = pred_confs[index]
+                        prob = np,amax(np.exp(pred_conf) / (np.sum(np.exp(pred_conf)) + 1e-5))
+                        pred_label = np.argmax(pred_conf)
+
+                        confs.append((box, prob, pred_label))
+                        index += 1
+        
+        return anchors
+
+
 
 class NotSpecifiedException(Exception):
     def __init__(self, name, epicenter):
@@ -21,3 +57,7 @@ class NotSpecifiedException(Exception):
         self.epicenter = epicenter
     def __str__(self):
         return ('Parameter "{0.name}" is not specified in "{0.epicenter}"'.format(self))
+
+
+matcher = Matcher([1, 1, 1, 1], [1, 1])
+print(matcher.apply_prediction(None, None))
