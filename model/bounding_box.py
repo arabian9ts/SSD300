12c8de2bba3518d4e5fccbc22b86a382d8242371
arabian9ts <nok14.tm59@gmail.com>
2017-10-05 15:15:15 +0900

    fix: box definition

--- model/bounding_box.py
+++ model/bounding_box.py
@@ -1,6 +1,6 @@
 """
 Bouding Box is the result of comparison with default box.
-bouding box has locs, argmax of softmax output and prediction.
+bouding box has loc and label.
 
 date: 10/4
 author: arabian9ts
@@ -10,34 +10,27 @@ from model.exception import *
 
 class Box:
     _locs = []
-    _prob = 0.0
     _index = 0
 
-    def __init__(self, locs, prob, index):
+    def __init__(self, loc, index):
         """
         initializer require all params
 
         Args:
             locs: where this box exists
-            prob: the most high rate of probability distribution
-            index: prob's index (meaning predicted label)
+            index: class label
         """
 
-        if not (locs and prob and index):
+        if not (loc and index):
             raise NotSpecifiedException('some args', '__init__ @Box')
             
-        if isinstance(locs, list) and 4 == len(locs):
-            self.locs = locs
-        if isinstance(prob, float):
-            self.prob = prob
+        if isinstance(loc, list) and 4 == len(loc):
+            self.loc = loc
         if isinstance(index, int):
             self.index = index
 
-    def locs(self):
-        return self._locs
-
-    def prob(self):
-        return self._prob
+    def loc(self):
+        return self._loc
 
     def index(self):
         return self._index
