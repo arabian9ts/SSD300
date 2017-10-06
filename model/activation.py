ddaf3a9d96e7fbf4a9397e50c9b9046a291a4b2b
arabian9ts <nok14.tm59@gmail.com>
2017-10-06 19:08:21 +0900

    rm: session process

--- model/activation.py
+++ model/activation.py
@@ -12,7 +12,3 @@ class Activation(enum.Enum):
     identity = tf.identity
     relu = tf.nn.relu
     softmax = tf.nn.softmax
-
-sess = tf.Session()
-sess.run(tf.global_variables_initializer())
-print(sess.run(Activation.relu(1)))
