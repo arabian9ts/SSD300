7bbea374508876593e7ca3b2f2ae724b0944cd6c
arabian9ts <nok14.tm59@gmail.com>
2017-10-07 16:54:49 +0900

    enhance: test process using test.jpg

--- train.py
+++ train.py
@@ -9,6 +9,7 @@ import tensorboard as tf
 import numpy as np
 
 from model.ssd import *
+from util.util import *
 from matcher import Matcher
 from model.default_box import *
 
@@ -16,7 +17,8 @@ from model.default_box import *
 
 input = tf.placeholder(shape=[None, 300, 300, 3], dtype=tf.float32)
 ssd = SSD()
-fmaps = ssd.build(input, is_training=True)
+fmaps, confs, locs = ssd.build(input, is_training=True)
+train_set = [fmaps, confs, locs]
 fmap_shapes = [map.get_shape().as_list() for map in fmaps]
 print('fmap shapes is '+str(fmap_shapes))
 dboxes = generate_boxes(fmap_shapes)
@@ -29,4 +31,10 @@ matcher = Matcher(fmap_shapes, dboxes)
 actual_labels = np.array([1, 2])
 actual_locs = np.array([[0.5, 0.5, 1, 1], [4, 4, 4, 4]])
 
-print(matcher.matching(ssd.pred_confs, ssd.pred_locs, actual_labels, actual_locs))
+with tf.Session() as sess:
+    sess.run(tf.global_variables_initializer())
+    image = load_image('./test.jpg')
+    image = image.reshape((1, 300, 300, 3))
+    feature_maps, pred_confs, pred_locs = sess.run(train_set, feed_dict={input: image})
+    print(len(pred_confs))
+    print(matcher.matching(pred_confs, pred_locs, actual_labels, actual_locs))
