c7d5110f0451430e57a0e7da2e0b4238d517cce4
arabian9ts <nok14.tm59@gmail.com>
2017-10-21 21:11:54 +0900

    fix: rect's elements in corner2center and center2corner

--- model/computation.py
+++ model/computation.py
@@ -10,12 +10,12 @@ def intersection(rect1, rect2):
     """
     intersecton of units
     compute boarder line top, left, right and bottom.
-    rect is defined as [ top_left_x, top_left_y, height, width ]
+    rect is defined as [ top_left_x, top_left_y, width, height ]
     """
     top = max(rect1[1], rect2[1])
     left = max(rect1[0], rect2[0])
-    right = min(rect1[0] + rect1[3], rect2[0] + rect2[3])
-    bottom = min(rect1[1] + rect1[2], rect2[1] + rect2[2])
+    right = min(rect1[0] + rect1[2], rect2[0] + rect2[2])
+    bottom = min(rect1[1] + rect1[3], rect2[1] + rect2[3])
 
     if bottom > top and right > left:
         return (bottom-top)*(right-left)
@@ -42,29 +42,21 @@ def jaccard(rect1, rect2):
     return intersect / union
 
 
-def swap_width_height(rect):
-    """
-    swap width and height of the specified rectangle
-    """
-
-    return [rect[0], rect[1], rect[3], rect[2]]
-
-
 def corner2center(rect):
     """
-    rect is defined as [ top_left_x, top_left_y, height, width ]
+    rect is defined as [ top_left_x, top_left_y, width, height ]
     """
-    center_x = rect[0] + rect[3] / 2
-    center_y = rect[1] + rect[2] / 2
+    center_x = rect[0] + rect[2] / 2
+    center_y = rect[1] + rect[3] / 2
 
     return [center_x, center_y, rect[2], rect[3]]
 
 
 def center2corner(rect):
     """
-    rect is defined as [ top_left_x, top_left_y, height, width ]
+    rect is defined as [ top_left_x, top_left_y, width, height ]
     """
-    corner_x = rect[0] - (rect[3] - rect[0])
-    corner_y = rect[1] - (rect[2] - rect[1])
+    corner_x = rect[0] - (rect[2] - rect[0])
+    corner_y = rect[1] - (rect[3] - rect[1])
 
     return [corner_x, corner_y, rect[2], rect[3]]
