fix: sk computation

model/default_box.py
@@ -50,18 +50,18 @@ def generate_boxes(fmap_shapes):
     default_boxes = []
 
     # this loop should be already 6 loops
-    for index, map_shape in zip(range(len(fmap_shapes)), fmap_shapes):
+    for index, map_shape in enumerate(fmap_shapes):
         height = map_shape[1]
         width = map_shape[2]
         ratios = box_ratios[index]
-        s_k = scale(index)
-        s_k1 = scale(index+1)
 
         for y in range(height):
             center_y = (y + 0.5) / float(height)
             for x in range(width):
                 center_x = (x + 0.5) / float(width)
                 for i, ratio in enumerate(ratios):
+                    s_k = scale(index)
+                    s_k1 = scale(index+1)
                     
                     if 0 == i:
                         s_k = np.sqrt(s_k*s_k1)